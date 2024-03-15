ce3c6bc6200ecae3f142635c91acd4f90249fe53
LeiKtl <leilaktl.pro@gmail.com>
2024-03-15 16:13:22 +0100

    🎨 Create contact page

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,14 @@
+import './contact.scss';
+
+function Contact () {
+
+    return (
+        <main className='container_contact'>
+            <h2 className='container_contact_title'>Let's<br/>collaborate</h2>
+            <a href="mailto:leilaktl.pro@gmail.com" className='container_contact_content'>leilaktl.pro@gmail.com<span className='container_contact_content-underline'></span></a>
+        </main> 
+    )
+      
+}
+
+export default Contact
