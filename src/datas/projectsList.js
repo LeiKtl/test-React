b51f6a5a33a69fb19dc2a5d82d26f1289557bf90
LeiKtl <leilaktl.pro@gmail.com>
2024-03-15 16:05:06 +0100

    🎉 Begin project with create-react-app

---
+++ src/datas/projectsList.js
@@ -0,0 +1,62 @@
+export const projectsList = [
+    {
+        "id": 1,
+        "number": "/1",
+        "title": "Booki",
+        "formation": "OpenClassrooms web integrator training course project.",
+        "description": "The company wanted to develop a website that allows its users to find accommodation and activities in the city of their choice. For this project I integrated the site interface in HTML and CSS, I had Figma models for mobile, tablet and desktop, as well as images and a summary note on the specifics of the project. This allowed me to develop essential skills in responsive design, integrate a site according to a model as well as install a front-end development environment.",
+        "picture": "booki.jpg",
+        "github" : "https://github.com/LeiKtl/OC-Project3",
+        "site" : "https://leiktl.github.io/OC-Project3/"
+    },
+    {
+        "id": 2,
+        "number": "/2",
+        "title": "Ohmyfood",
+        "formation": "OpenClassroom web integrator training course project.",
+        "description": "For this project, the client wanted a website that lists the menus of gourmet restaurants, the mission was to integrate the site in mobile-first, as well as to make the tablet and desktop version in accordance with the Figma models which were provided, while using SASS and including animations to enrich the user experience of the site, all by versioning the site on GitHub. This allowed me to develop skills in mobile-first responsive design, use of SASS as well as the use of GitHub.",
+        "picture": "ohmyfood.jpg",
+        "github" : "https://github.com/LeiKtl/OC-Projet4",
+        "site" : ""
+    },
+    {
+        "id": 3,
+        "number": "/3",
+        "title": "Print-it",
+        "formation": "OpenClassroom web integrator training course project.",
+        "description": "For this project the client, Print-in which is a printing company, wanted to dynamize their static website by adding an interactive carousel. So my mission was to update the existing HTML code and add a carousel in JavaScript. This project allowed me to work on the fundamentals of JavaScript.",
+        "picture": "print_it.jpg",
+        "github" : "https://github.com/LeiKtl/OC-Projet6",
+        "site" : "https://leiktl.github.io/OC-Projet5/"
+    },
+    {
+        "id": 4,
+        "number": "/4",
+        "title": "Sophie Bluel",
+        "formation": "OpenClassroom web integrator training course project.",
+        "description": "For this portfolio site of an interior designer: Sophie Bluel, the mission was to integrate the dynamic part of the site, such as the creation of a gallery, the creation of a user login page as well as the possibility of managing images (add or delete), using javascript language and an API. This project allowed me to strengthen my knowledge of JavaScript as well as learn to communicate with a database.",
+        "picture": "sophie-bluel.png",
+        "github" : "https://github.com/LeiKtl/OC-Projet6",
+        "site" : ""
+    },
+    {
+        "id": 5,
+        "number": "/5",
+        "title": "Kasa",
+        "formation": "OpenClassroom web integrator training course project.",
+        "description": "For this project, the company wanted a entire redesign of their website and implement the front-end of the application using React, Create-React-App, React Router and Sass;they also wanted to add animations to improve the user experience. This project allowed me to learn programming with React and therefore to develop my code into components.",
+        "picture": "kasa.jpg",
+        "github" : "https://github.com/LeiKtl/OC-Project8",
+        "site" : ""
+    },
+    {
+        "id" : 6,
+        "number" : "/6",
+        "title" : "Nina Carducci",
+        "formation" : "OpenClassroom web integrator training course project.", 
+        "description" : "",
+        "picture" : "nina.webp",
+        "github" : "https://github.com/LeiKtl/OC-Project9",
+        "site" : "https://leiktl.github.io/OC-Project9/"
+    }
+]
