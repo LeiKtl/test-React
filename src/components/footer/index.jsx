3c9fc9a1cb9e0c6aca34eca498a60b3cc3de029e
LeiKtl <leilaktl.pro@gmail.com>
2024-03-15 16:08:53 +0100

    🎨 Create footer component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,21 @@
+import './footer.scss';
+import githubLogo from '../../assets/logo_github.png';
+import linkedinLogo from '../../assets/logo_linkedin.png'
+import { Link } from 'react-router-dom';
+
+function Header () {
+    return (
+        <footer className='footer'>
+            <Link to="/contact" className='footer_contact'>Let's<br/>collaborate</Link>
+            <div className='footer_container'>
+                <Link to="https://github.com/LeiKtl?tab=repositories">
+                    <img className='footer_container_logo1' src={githubLogo} alt='Github Logo'/>
+                </Link>
+                <img className='footer_container_logo2' src={linkedinLogo} alt='Linkedin Logo'/>
+            </div>
+            {/* <p className='footer_content'>Designed by Leï. - <Link>Mentions Légales</Link></p> */}
+        </footer>
+    )
+};
+
+export default Header;
