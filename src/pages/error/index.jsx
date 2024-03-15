cc3b583ed5d7ee90d8a57d39724fa85dc6e60133
LeiKtl <leilaktl.pro@gmail.com>
2024-03-15 16:13:35 +0100

    🎨 Create error page

---
+++ src/pages/error/index.jsx
@@ -0,0 +1,14 @@
+import './error.scss';
+import { Link } from 'react-router-dom';
+
+function Error () {
+    return (
+        <div className='container_error'>
+            <span className='container_error_number'>404</span>
+            <p className='container_error_content'>We can't find the page you're looking for..</p>
+            <Link  to="/" className='container_error_button'>home page</Link>
+        </div>
+    )
+}
+
+export default Error;
